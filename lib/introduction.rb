2608c686c3717f7cb76d6a4fdea3463007788921
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-28 20:35:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,11 @@
-#First, define a method called "introduction" that takes in a name and ouputs the phrase "Hi, my name is "name" "
+#First, define a method called "introduction" that takes in a name and ouputs the phrase "Hi, my name is (name")."
 
 def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-#Second, define a method called "introduction" that takes in a name and ouputs the phrase "Hi, my name is "name" "
+#Second, define a method called "introduction_with_language" that takes in a name and a language and ouputs the phrase "Hi, my name is (name) and I am learning to program in (language)."
+
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
