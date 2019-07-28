fd1be4be116e5af6ac6dadeae8efaa4947be2eb6
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-28 20:41:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,4 +14,6 @@ end
 
 def introduction_with_language_optional (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  if language = nil
+    puts "Ruby"
 end
